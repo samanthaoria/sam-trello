e46012cc1e63c71bdfe53f248eb01bce73bff155
Hedley Luna <hedleygois@gmail.com>
2022-12-08 22:21:00 +0100

    First OpenAPI configuration (#2)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sam Trello')
+    .setDescription("Sam's Trello API")
+    .setVersion('0.1')
+    .addTag('sam')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('document', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
